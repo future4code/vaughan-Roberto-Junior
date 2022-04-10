4433b46ccfd78d31b4dce7a2b09d9ca67d8afec2
MaiaJrDev <rm.dcjr@hotmail.com>
2022-04-09 21:13:44 -0300

    6,7,8

--- modulo4/toDoList/src/index.ts
+++ modulo4/toDoList/src/index.ts
@@ -19,6 +19,7 @@ const server = app.listen(process.env.PORT || 3003, () => {
 });
 
 
+//Exercicio 1
 //Endpoint para criar usuario
 app.post('/post', async(req, res) => {
       const {name, nickname, email} = req.body;
@@ -54,6 +55,7 @@ const createUser = async(name: string, nickname: string, email: string): Promise
 }
 
 
+//Exercicio2
 //Endpoint para pegar um usuario por id
 app.get('/user/:id', async(req, res) => {
 
@@ -86,6 +88,8 @@ const getUser = async(id: string): Promise<any> => {
 
 }
 
+
+//Exercicio 3
 //Endpoint para editar usuario
 app.put('/user/edit/:id', async(req, res) => {
 
@@ -126,6 +130,7 @@ const editUser = async(id: string, name?: string, nickname?: string): Promise<an
 }
 
 
+//Exercicio 4
 //Endpoint para Criar tarefa
 app.post('/task', async(req, res) => {
 
@@ -169,6 +174,7 @@ const createTask = async(title: string, description: string, limitDate: string, 
 }
 
 
+//Exercicio 5
 //Endpoint para pehar tarefa pelo id
 app.get('/task/:id', async(req, res) => {
 
@@ -200,10 +206,138 @@ app.get('/task/:id', async(req, res) => {
 const consultTask = async(id: string): Promise<any> => {
   
   const task = await connection('TodoListTask')
-  .select('*')
-  .where({id: id})
+  .join('TodoListUser', 'TodoListUser.id', 'TodoListTask.creator_user_id')
+  .select('TodoListTask.*', 'TodoListUser.nickname')
+  .where({'TodoListTask.id': id});
 
   return task;
 
 }
 
+
+
+//Desafio 6
+//Endpoint para pegar todos os usuarios
+app.get('/users/all', async(req, res) => {
+
+  try {
+
+    const allUsers = await getAllUsers();
+
+    if(allUsers.length === 0){
+      res.send(allUsers);
+    }
+
+    let usersFomat = {users: allUsers}
+
+    res.status(200).send(usersFomat);
+
+  }catch(err: any){
+
+    res.status(500).send(err.sqlMessage || err.message);
+
+  }
+})
+
+const getAllUsers = async(): Promise<any> => {
+    
+    const response = await connection('TodoListUser')
+                           .select('id', 'nickname');
+
+    return response;
+
+}
+
+
+//Desafio 7
+//Endpoint para  Pegar tarefas criadas por um usuário;
+
+app.get('/tasks', async(req, res) => {
+
+  const id = req.query.creatorUserId as string;
+
+  try {
+
+      if(!id){
+        throw new Error('Deve ser enviado um id para consulta !')   
+      }
+
+      const response = await getTaskById(id);
+
+      if(response.length === 0){
+        res.status(200).send(response);
+      }
+
+      let responseFormatDate = response.map((item: any) => {
+        item.limit_date = item.limit_date.toLocaleDateString();
+        return item;
+      })
+
+      let responseFormat = {tasks: responseFormatDate}
+   
+      res.send(responseFormat);
+
+
+  }catch(err: any){
+
+    res.status(500).send(err.sqlMessage || err.message);
+
+  }
+})
+
+const getTaskById = async(id: string): Promise<any> => {
+    
+    const response = await connection('TodoListTask')
+                           .join('TodoListUser', 'TodoListUser.id', 'TodoListTask.creator_user_id')
+                           .select('TodoListTask.*', 'TodoListUser.nickname')
+                           .where({creator_user_id: id});
+
+    return response;
+
+}
+
+
+//Desafio 8
+//Pesquisar Usuario
+app.get('/users', async(req, res) => {
+
+  const search = req.query.query as string;
+
+  try {
+
+      if(!search){
+        throw new Error('Deve ser enviado um nome para a busca !')   
+      }
+
+      const response = await getUserSearch(search);
+
+      if(response.length === 0){
+        res.send(response);
+      }else{
+        
+        let newResponse = {users: response}
+   
+        res.send(newResponse);
+
+      }
+
+   
+
+
+  }catch(err: any){
+
+    res.status(500).send(err.sqlMessage || err.message);
+
+  }
+})
+
+const getUserSearch = async(search: string): Promise<any> => {
+
+   const response = await connection('TodoListUser')
+                          .select('id' , 'nickname')
+                          .whereILike('nickname', `%${search}%`)
+                          .orWhereILike('email', `%${search}%`);
+    
+    return response;
+
+}
